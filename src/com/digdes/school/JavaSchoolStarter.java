1531729d18a762ce4c35822bd2168fb832283f94
SerebrovEV <89818121788@mail.ru>
2023-03-21 16:08:25 +0300

    in progress

--- src/com/digdes/school/JavaSchoolStarter.java
+++ src/com/digdes/school/JavaSchoolStarter.java
@@ -1,16 +1,37 @@
 package com.digdes.school;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class JavaSchoolStarter {
     public JavaSchoolStarter() {
     }
+    private ArrayList<Map<String, Object>> dataBase;
 
     public List<Map<String, Object>> execute(String request) throws Exception {
 
+        String[] question = request.split(",");
+        String command = question[0].toLowerCase();
+        if (command.startsWith("insert")) {
+            System.out.println(Arrays.toString(question));
+        } else if (command.startsWith("update")) {
+            System.out.println(Arrays.toString(question));
+        } else if (command.startsWith("delete")) {
+            System.out.println(Arrays.toString(question));
+        } else if (command.startsWith("select")) {
+            System.out.println(Arrays.toString(question));
+        } else {
+            throw new IllegalArgumentException();
+        }
         return new ArrayList<>();
     }
+
+    private ArrayList<Map<String, Object>> addRow(String[] strs) {
+        Map<String, Object> newRow = new HashMap<>();
+
+
+
+        return new ArrayList<>();
+    }
+
 
 }
--- src/com/digdes/school/Main.java
+++ src/com/digdes/school/Main.java
@@ -1,9 +1,29 @@
 package com.digdes.school;
 
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        JavaSchoolStarter starter = new JavaSchoolStarter();
+        List<String> commands = List.of(
+                "INSERT VALUES 'lastName' = 'Федоров' , 'id'=3, 'age'=40, 'active'=true",
+                "UPDATE VALUES 'active'=false, 'cost'=10.1 where 'id'=3",
+                "UPdATE VALUES 'active'=false, 'cost'=10.1 where 'id'=3",
+                "Delete VALUES 'active'=false, 'cost'=10.1 where 'id'=3",
+                "Select VALUES 'active'=false, 'cost'=10.1 where 'id'=3");
+        try {
+
+            for (String str : commands) {
+                starter.execute(str);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            System.out.println("Done");
+        }
 
     }
 }
