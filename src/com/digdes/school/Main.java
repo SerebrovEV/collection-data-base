4ff887750b8dd3acdf4f0fbaedc2015b8e353658
evgen <89818121788@mail.ru>
2023-03-23 22:04:06 +0300

    In progress 23/03

--- src/com/digdes/school/Main.java
+++ src/com/digdes/school/Main.java
@@ -9,7 +9,14 @@ public class Main {
 
         JavaSchoolStarter starter = new JavaSchoolStarter();
         List<String> commands = List.of(
-                "INSERT VALUES 'LastName' = ' Федоров' , 'id'= 3, 'age'= 40, 'active'= true",
+                "INSERT VALUES 'LastName' = ' Федоров' , 'id'= 3, 'age'= 402, 'active'= true, 'cost'=4.5",
+                "INSERT VALUES 'LastName' = ' Федоров' , 'id'= 3, 'age'= 402, 'active'= true, 'cost'=4.7",
+                "INSERT VALUES 'LastName' = ' Федоров' , 'id'= 3, 'age'= 402, 'active'= true, 'cost'=4.8",
+                "INSERT VALUES 'LastName' = ' Федоров2' , 'id'= 37, 'age'= 41, 'active'= false",
+                "INSERT VALUES 'LastName' = ' Федоровa' , 'id'= 33, 'age'= 10, 'active'= true",
+                "INSERT VALUES 'LastName' = ' Федоровsaf' , 'id'= 345, 'age'= 480, 'active'= false",
+                "INSERT VALUES 'LastName' = ' Федороfsdв' , 'id'= 34, 'age'= 40, 'active'= true",
+                "INSERT VALUES 'LastName' = ' Федоровdfs' , 'id'= 37, 'age'= 430, 'active'= true",
                 //  "UPDATE VALUES 'active'=false, 'cost'=10.1 where 'id'=3",
                 //   "UPdATE VALUES 'active'=false, 'cost'=10.1 where 'id'=3",
                 "Delete where 'id' != 3 and 'name' > 10"
